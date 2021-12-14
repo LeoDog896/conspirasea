9670e4eda6809b66b76ed6502da7893cda42ffbb
LeoDog896 <LeoDog896@hotmail.com>
2021-12-14 13:05:23 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ModeSwitcher from './ModeSwitcher.svelte';
+	import Tailwindcss from './Tailwindcss.svelte';
+	import { draggable } from "svelte-drag"
+	export let name: string;
+
+	interface Element {
+		
+	}
+</script>
+<Tailwindcss />
+<ModeSwitcher />
