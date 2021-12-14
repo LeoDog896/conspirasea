ea2adcfba936c5871f480e76f0da0adfa9e2716d
LeoDog896 <LeoDog896@hotmail.com>
2021-12-14 13:46:15 +0000

    Base tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,25 @@
 	import ModeSwitcher from './ModeSwitcher.svelte';
 	import Tailwindcss from './Tailwindcss.svelte';
 	import { draggable } from "svelte-drag"
-	export let name: string;
 
 	interface Element {
-		
+		imageURL?: string,
+		content?: string
 	}
+
+	let elements: Element[] = [
+		{
+			content: "Hello World"
+		},
+		{
+			content: "Testing 1-2-3"
+		}
+	]
 </script>
 <Tailwindcss />
 <ModeSwitcher />
+{#each elements as element}
+	<div use:draggable class="w-px z-0 p-6 shadow-md bg-white dark:bg-black">
+		<span class="text-black dark:text-white">{element.content}</span>
+	</div>
+{/each}
