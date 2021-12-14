9471ba46525d65d77441575e8d8afb3fa32c3481
LeoDog896 <LeoDog896@hotmail.com>
2021-12-14 18:46:40 +0000

    Begin making conspiracies

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import ModeSwitcher from './ModeSwitcher.svelte';
 	import Tailwindcss from './Tailwindcss.svelte';
 	import { draggable } from "svelte-drag"
+	import Modal from 'svelte-simple-modal';
 
 	interface Element {
 		imageURL?: string,
@@ -17,10 +18,13 @@
 		}
 	]
 </script>
-<Tailwindcss />
-<ModeSwitcher />
-{#each elements as element}
-	<div use:draggable class="w-px z-0 p-6 shadow-md bg-white dark:bg-black">
-		<span class="text-black dark:text-white">{element.content}</span>
-	</div>
-{/each}
+<Modal>
+	<Tailwindcss />
+	<ModeSwitcher />
+	<button class="px-4 py-2 justify-center w-full shadow-sm bg-blue-200 dark:bg-blue-900 fixed top-0">Add</button>
+	{#each elements as element}
+		<div use:draggable class="w-24 z-0 p-6 shadow-md bg-white dark:bg-black">
+			<span class="text-black dark:text-white">{element.content}</span>
+		</div>
+	{/each}
+</Modal>
