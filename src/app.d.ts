d7449d1af8ed50696635abe1b94bfdefa866e942
LeoDog896 <LeoDog896@hotmail.com>
2022-04-27 15:27:48 +0000

    Set up base conspirasea

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference types="@neodrag/svelte/globals" />
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import { draggable } from "@neodrag/svelte"
-  import { tick } from "svelte"
+  import { onMount, tick } from "svelte"
+
+  let width: number;
+  let height: number;
+  let canvas: HTMLCanvasElement | null = null;
+
+  onMount(() => {
+    width = window.innerWidth
+    height = window.innerHeight
+  })
 
 	interface Position {
 		x: number,
@@ -8,12 +17,16 @@
 	}
 
 	interface Element {
-		imageURL?: string,
-		content?: string,
-		editable?: boolean,
-		position?: Position,
-    selected?: boolean
-		textarea?: HTMLTextAreaElement
+		imageURL?: string;
+		content: string;
+    id: string;
+		editable?: boolean;
+		position: Position;
+    /** An array of other nodes they're connected to. */
+    connections: string[];
+    selected?: boolean;
+    self?: HTMLElement;
+		textarea?: HTMLTextAreaElement;
 	}
 
 	function addElement(event: MouseEvent) {
@@ -22,26 +35,78 @@
 				x: event.pageX,
 				y: event.pageY
 			},
+      connections: [],
+      id: crypto.randomUUID(),
 			content: "My conspiracy note"
 		}]
 	}
 
-	let elements: Element[] = []
+  let elements: Element[] = []
+
+  $: if (elements.length > 0 && canvas && canvas.getContext("2d")) {
+    const context = canvas.getContext("2d")!
+    context.strokeStyle = "rgba(235, 64, 52, 0.3)"
+    context.lineWidth = 4
+    context.clearRect(0, 0, width, height);
+    elements.forEach(it => it.connections.forEach(connection => {
+      const elementSelection = elements.filter(it => it.id == connection)
+      if (elementSelection.length === 0) return
+      const element = elementSelection[0]
+      context.beginPath()
+      context.moveTo(
+        it.position.x + (it.self?.clientWidth || 0) / 2,
+        it.position.y + (it.self?.clientHeight || 0) / 8
+      )
+      context.lineTo(
+        element.position.x + (element.self?.clientWidth || 0) / 2,
+        element.position.y + (element.self?.clientHeight || 0) / 8
+      )
+      context.stroke()
+    }))
+  }
+
 </script>
-<div id="container" class="fixed h-screen w-screen" on:dblclick|self={addElement}>
+<svelte:window on:resize={() => {
+  width = window.innerWidth
+  height = window.innerHeight
+}}></svelte:window>
+<div id="container" 
+class="fixed top-0 left-0 h-screen w-screen bg-gray-300" 
+on:click|self={() => {
+  elements = elements.map(it => {
+    it.selected = false;
+    return it;
+  })
+}}
+on:dblclick|self={addElement}
+>
 	{#each elements as element}
-		<div 
+		<div
+      bind:this={element.self} 
 			on:dblclick={() => {
         element.editable = !element.editable;
         tick().then(() => element.textarea?.focus())
       }}
-      on:click={() => element.selected = !element.selected}
-			use:draggable={{axis: "both", bounds: "#container", position: element.position, defaultClassDragging: "box-dragging"}}
-      on:neodrag:end={({ offsetX, offsetY }) => {
-        element.position.x += offsetX;
-        element.position.y += offsetY;
+      on:click={() => {
+        const otherSelectedElement = elements.filter(it => it !== element).filter(it => it.selected);
+        if (otherSelectedElement.length !== 0) {
+          elements[elements.indexOf(otherSelectedElement[0])].selected = false
+          element.connections = [...element.connections, otherSelectedElement[0].id]
+          elements = elements
+          return
+        }
+
+        element.selected = !element.selected
       }}
-			class="transition-shadow fixed max-w-md z-0 p-6 shadow-lg rounded-md {element.selected ? "bg-gray-100" : "bg-gray-50"} dark:bg-black"
+			use:draggable={{ axis: "both", bounds: "#container", position: element.position, defaultClassDragging: "box-dragging" }}
+      on:neodrag={event => {
+        if (!event || !event.detail) return
+        const { offsetX, offsetY } = event.detail;
+        element.position.x = offsetX;
+        element.position.y = offsetY;
+      }}
+			class="transition-shadow fixed max-w-md z-0 p-6 shadow-lg rounded-md
+      {element.selected ? "bg-gray-200" : "bg-white"} dark:bg-black"
 		>
 			{#if element.editable}
 				<textarea
@@ -68,3 +133,4 @@
 		@apply shadow-xl;
 	}
 </style>
+<canvas class="fixed z-10 top-0 left-0 pointer-events-none" bind:this={canvas} {width} {height}/>
