59d405e59b534e7643d8c0261701596aff42353a
LeoDog896 <LeoDog896@hotmail.com>
2022-05-05 16:34:41 +0000

    Seperate canvas from logic

--- src/lib/TopBar.svelte
+++ src/lib/TopBar.svelte
@@ -2,9 +2,11 @@
   import { fly } from "svelte/transition"
   import ConspiraseaList from "./ConspiraseaList.svelte"
   import { currentConspirasea } from "./elements"
+  import type Canvas from "./Canvas.svelte"
 
   let topBarVisible = true
   let listVisible = false
+  export let canvas: Canvas
 </script>
 {#if topBarVisible}
   <div transition:fly={{ y: -150 }} class="fixed top-0 left-0 w-screen p-4 bg-white shadow-lg rounded-b-lg">
@@ -13,7 +15,10 @@
     <button class="bg-cyan-200 px-4 py-2 rounded-lg mx-2 hover:bg-cyan-300 transition-all active:bg-cyan-400 active:ring ring-cyan-100">Load</button>
     <button class="bg-cyan-200 px-4 py-2 rounded-lg mx-2 hover:bg-cyan-300 transition-all active:bg-cyan-400 active:ring ring-cyan-100" on:click={() => {
       const result = confirm("Are you sure you want to clear the elements?");
-      if (result) $currentConspirasea.elements = []
+      if (result) {
+        $currentConspirasea.elements = []
+        canvas.draw()
+      }
     }}>Clear</button>
   </div>
 {/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
 	import { draggable } from "@neodrag/svelte"
-  import { onMount, tick } from "svelte"
+  import { tick } from "svelte"
   import TopBar from "$lib/TopBar.svelte"
-  import { currentConspirasea, conspiraseas } from "$lib/elements"
+  import { currentConspirasea } from "$lib/elements"
+  import Canvas from "$lib/Canvas.svelte"
 
-  let width: number;
-  let height: number;
-  let canvas: HTMLCanvasElement | null = null;
-
-  onMount(() => {
-    width = window.innerWidth
-    height = window.innerHeight
-  })
+  let canvas: typeof Canvas
 
 	function addElement(event: MouseEvent) {
 		$currentConspirasea.elements = [...$currentConspirasea.elements, {
@@ -25,47 +19,7 @@
 		}]
 	}
 
-  function draw() {
-    if (!canvas || !canvas.getContext("2d")) return
-
-    const context = canvas.getContext("2d")!
-    context.clearRect(0, 0, width, height);
-    $currentConspirasea.elements.forEach(it => it.connections.forEach(connection => {
-      const elementSelection = $currentConspirasea.elements.filter(it => it.id == connection)
-      if (elementSelection.length === 0) return
-      const element = elementSelection[0]
-      context.strokeStyle = "rgba(235, 64, 52, 0.3)"
-      context.lineWidth = 4
-      context.beginPath()
-      const itX = it.position.x + (it.self?.clientWidth || 0) / 2
-      const itY = it.position.y + (it.self?.clientHeight || 0) / 8
-      const elementX = element.position.x + (element.self?.clientWidth || 0) / 2
-      const elementY = element.position.y + (element.self?.clientHeight || 0) / 8
-      context.moveTo(itX, itY)
-      context.lineTo(elementX, elementY)
-      context.stroke()
-
-      context.strokeStyle = "rgba(0, 0, 0, 0)"
-      context.fillStyle = "#ebde34"
-      context.beginPath();
-      context.arc(itX, itY, 7, 0, 2 * Math.PI);
-      context.fill();
-      context.beginPath();
-      context.arc(elementX, elementY, 7, 0, 2 * Math.PI);
-      context.fill();
-    }))
-  }
-
-  $: if (width && height && $conspiraseas && $currentConspirasea.elements.length > 0 && canvas && canvas.getContext("2d")) {
-    draw()
-  }
-
 </script>
-<svelte:window on:resize={() => {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  tick().then(draw)
-}}></svelte:window>
 <div id="container" 
 class="fixed top-0 left-0 h-full w-screen bg-gray-300" 
 on:click|self={() => {
@@ -133,5 +87,5 @@ on:dblclick|self={addElement}
 		@apply shadow-xl;
 	}
 </style>
-<canvas class="fixed top-0 left-0 pointer-events-none" bind:this={canvas} {width} {height}/>
-<TopBar></TopBar>
+<TopBar bind:canvas={canvas}/>
+<Canvas bind:this={canvas}/>
